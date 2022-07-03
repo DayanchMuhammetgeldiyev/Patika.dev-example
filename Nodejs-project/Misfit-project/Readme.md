fb18fddfab548e77cece91006baa615eea2e3b21
Dayanch  Muhammetgeldiyev <66455000+DayanchMuhammetgeldiyev@users.noreply.github.com>
2022-07-03 20:30:43 +0300

    Patika.dev

---
+++ Nodejs-project/Misfit-project/Readme.md
@@ -0,0 +1,5 @@
+<img
+  src="public/readmeimg/Home.png"
+  alt="Alt text"
+  title="Optional title"
+  style="display: inline-block; margin: 0 auto; max-width: 300px">
---
+++ Nodejs-project/Misfit-project/routes/pageRoute.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const pageController = require("../controllers/pageController");
+
+// routes
+router.route("/").get(pageController.getIndexPage);
+router.route("/about").get(pageController.getAboutPage);
+router.route("/contact").get(pageController.getContactPage);
+router.route("/gallery").get(pageController.getGalleryPage);
+router.route("/trainer").get(pageController.getTrainerPage);
+router.route("/login").get(pageController.getLoginPage);
+router.route("/register").get(pageController.getResgisterPage);
+module.exports = router;
